91d77f6d98711fb5d22a62e909b08ccc02a54f25
family <family@familys-iMac.local>
2023-05-17 12:31:59 -0400

    Adds styling for tablet

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div className="footer">
       <p className="email">real.edgemere.clt@gmail.com</p>
+      <p className="site-author">2023 Tay Sites</p>
     </div>
   );
 }
