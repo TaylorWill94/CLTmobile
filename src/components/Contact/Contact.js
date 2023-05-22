f95b4bf37acc819d3a0db392c5071c1d37d5648e
family <family@familys-iMac.local>
2023-05-22 14:49:54 -0400

    Adds contact form functionalit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,39 +1,71 @@
 import "./Contact.css";
+import React, { useState } from "react";
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:3001/send-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="contact">
       <div className="contact-sec">
-        {/* <h1 className="contact-bio">real.edgemere.clt@gmail.com</h1> */}
-
-        <form>
-          {/* <form onSubmit={handleSubmit}> */}
+        <form onSubmit={handleSubmit}>
           <div className="form-sec">
-            <label>Name</label>
+            <label>Name *</label>
             <input
               type="text"
               name="name"
-              // value={formData.name}
-              // onChange={handleChange}
+              value={formData.name}
+              onChange={handleChange}
               required
             />
           </div>
           <div className="form-sec">
-            <label>Email</label>
+            <label>Email *</label>
             <input
               type="email"
               name="email"
-              // value={formData.email}
-              // onChange={handleChange}
+              value={formData.email}
+              onChange={handleChange}
               required
             />
           </div>
           <div className="form-sec">
-            <label>Message</label>
+            <label>Message *</label>
             <textarea
               name="message"
-              // value={formData.message}
-              // onChange={handleChange}
+              value={formData.message}
+              onChange={handleChange}
               required
             ></textarea>
           </div>
