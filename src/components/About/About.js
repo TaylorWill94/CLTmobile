da19c52083798ccfe721622e4cb5f54d9378eb89
Taylor <taylor@Taylors-MacBook-Pro.local>
2023-05-24 11:20:05 -0400

    Changes wording on About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,18 +7,18 @@ function About() {
       <div className="about-sec">
         <h1 className="about-bio">
           <span style={{ color: "orange" }}>
-            We are a new non-profit community organization, dedicated to
+            We are a new non-profit community organization, dedicated toß
             community control of land in Edgemere:
           </span>{" "}
           <br />
           <br />
-          Formed by local residents to bid on 116 HPD owned lots (through a
-          competitive process)– so our community can have a REAL say in how the
-          lots are developed! We were selected by HPD as the winner in September
-          2022! We’re doing things a little up-side-down! Most CLTs get
-          organized first, and THEN get land… so we really gotta get organized
-          to do this right! We need YOUR help to make community control in
-          Edgemere a success!
+          Formed by local residents who successfully bided on 119 vacant-City
+          owned HDP lots (through a competitive process)– so our community can
+          have a REAL say in how the lots are developed! We were selected by HPD
+          as the winner in Septemberß 2022! We’re doing things a little
+          up-side-down! Most CLTs get organized first, and THEN get land… so we
+          really gotta get organized to do this right! We need YOUR help to make
+          community control in Edgemere a success!
         </h1>
         <img className="plans-img" src={plans} alt="shows community plans" />
       </div>
