2a0e8d30f6b4c63945290af5104ef2f79ae12be1
Taylor <taylor@Taylors-MacBook-Pro.local>
2023-06-06 16:35:35 -0400

    Updates mission statement

--- src/components/Mission/Mission.js
+++ src/components/Mission/Mission.js
@@ -7,16 +7,17 @@ function Mission() {
       <div className="mission-sec">
         <h1 className="mission-bio">
           <span style={{ color: "orange" }}>
-            The CLT will support the revitalization of a marginalized community
-            of color through the development of affordable housing and
-            programming of land.
+            The ReAL Edgemere Community Land Trust is a community-led and
+            community-focused non-profit.
           </span>{" "}
           <br />
           <br />
-          The CLT will own, develop and manage land in order to create a just,
-          equitable and resilient Edgemere. The CLT will use grassroots methods
-          to plan and develop housing, open-spaces, and amenities to cultivate
-          economic, ecological, and cultural vibrancy.
+          The CLT will support the revitalization of a marginalized community of
+          color through the development of affordable housing and programming of
+          land. The CLT will own, develop and manage land in order to create a
+          just, equitable and resilient Edgemere. The CLT will use grassroots
+          methods to plan and develop housing, open-spaces, and amenities to
+          cultivate economic, ecological, and cultural vibrancy.
         </h1>
         <Link to="/about">
           <button className="read-more">read more</button>
